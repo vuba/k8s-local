d449e04b938c0ccaffd8b267dabbb411d9fdf77d
vuba <vuba.git@outlook.com>
2018-05-08 20:32:41 +0200

    [FIXED] setUpScript

--- setUpScript.sh
+++ setUpScript.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 echo "This script will provision the host and two nodes"
-echo "=========Provisioning host========="
+echo "================Provisioning host==============="
+echo "====Piping logs to host/host_provisionig.log===="
 cd host
 vagrant up > host_provisionig.log
 cd ..
-echo "===Provisioning host - FINISHED ==="
-echo "========Provisioning node-1========"
+echo "==========Provisioning host - FINISHED=========="
+echo "=============Provisioning node-1================"
+echo "==Piping logs to node-1/node-1_provisionig.log=="
 cd node-1
 vagrant up > node-1_provisionig.log
 cd ..
-echo "==Provisioning node-1 - FINISHED =="
-echo "========Provisioning node-1========"
+echo "=========Provisioning node-1 - FINISHED========="
+echo "=============Provisioning node-2================"
+echo "==Piping logs to node-2/node-2_provisionig.log=="
 cd node-2
 vagrant up > node-2_provisionig.log
-echo "==Provisioning node-2 - FINISHED =="
+echo "=========Provisioning node-2 - FINISHED========="
