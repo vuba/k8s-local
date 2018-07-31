2626b083236f5e510af751e6d1a9fafb22c12199
vuba <vuba.git@outlook.com>
2018-07-31 21:38:32 +0200

    [ADDED] script to destroy vagrant boxes

---
+++ vagrant/destroyVms.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+mkdir logs
+MACHINES=rancher,host,registry,nfs,minio,node-1,node-2
+START_TIME=time
+echo "Setting machine to destroy"
+echo "Machines to be destroy:["
+echo $MACHINES
+echo "]"
+for virtual_machines in $MACHINES
+do
+  echo "---- DESTROYING $virtual_machines ----"
+  cd $virtual_machines
+  vagrant destroy -f
+  cd ..
+done
+END_TIME=time
+echo "---- DONE DESTROYING ----"
+EXECUTION_TIME=($START_TIME-$END_TIME)/1000
+echo "---- EXECUTION TIME $EXECUTION_TIME seconds ----"
