e5faefab7183fc42d5767424ddc36a79b4c3634e
vuba <vuba.git@gmail.com>
2018-09-07 15:18:35 +0200

    [CHANGED] a lot

---
+++ runThis.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cd vagrant
+./provisionVms.sh
+cd ..
+./removeSshEntries.sh
+cd ansible
+ansible-playbook -i hosts dist-upgrade.yml
+ansible-playbook -i hosts configure-k8s-user.yml
+ansible-playbook -i hosts install-k8s-docker.yml
+ansible-playbook -i hosts install-k8s-docker.ym
+ansible-playbook -i hosts install-rancher.yml
