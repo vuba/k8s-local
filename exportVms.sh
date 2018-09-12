8a853ee2a3dfaa80a65e434711b099de951f6500
vuba <vuba.git@gmail.com>
2018-09-12 14:26:27 +0200

    [REMOVED] unnecessary scripts

--- exportVms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-VBoxManage export k8s-host -o vms/k8s-host.ova
-VBoxManage export k8s-rancher -o vms/k8s-rancher.ova
-VBoxManage export k8s-node-1 -o vms/k8s-node-1.ova
-VBoxManage export k8s-registry -o vms/k8s-registry.ova
-VBoxManage export k8s-minio -o vms/k8s-minio.ova
-VBoxManage export k8s-nfs -o vms/k8s-nfs.ova
--- runThis.sh
+++ runThis.sh
@@ -7,5 +7,6 @@ cd ansible
 ansible-playbook -i hosts dist-upgrade.yml
 ansible-playbook -i hosts configure-k8s-user.yml
 ansible-playbook -i hosts install-k8s-docker.yml
-ansible-playbook -i hosts install-k8s-docker.ym
+ansible-playbook -i hosts install-k8s-docker.yml
 ansible-playbook -i hosts install-rancher.yml
+cd ..
